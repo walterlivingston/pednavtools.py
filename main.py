34b484588b47215492c75cdb5f76812718fb071d
Walter Livingston <wblivingston18@gmail.com>
2024-03-03 11:51:07 -0600

    initial commit; madgwick

---
+++ main.py
@@ -0,0 +1,56 @@
+import numpy as np
+from numpy import linalg as la
+import quaternion
+from bagpy import bagreader
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import postprocessing_suite as pps
+import postprocessing_suite.ahrs as ahrs
+
+# Gains & Thresholds
+dt = (1/50)
+beta = 0.1
+zeta = 0.04
+mag_th = 0.15
+
+# Load Data
+b = bagreader('data/11-28-23_nav_side.bag')
+vecnav_IMU = b.message_by_topic('/vectornav/IMU')
+vecnav_Mag = b.message_by_topic('/vectornav/Mag')
+ublox_GPS = b.message_by_topic('/ublox/enuVelocityTagged')
+imu_data = pd.read_csv(vecnav_IMU)
+mag_data = pd.read_csv(vecnav_Mag)
+gps_data = pd.read_csv(ublox_GPS)
+
+enu_vel = np.array([gps_data['enuVelocity.eastVelocity'], gps_data['enuVelocity.northVelocity'], gps_data['enuVelocity.upVelocity']])
+course = np.arctan2(enu_vel[0,:], enu_vel[1,:])
+
+acc = np.array([imu_data['linear_acceleration.x'], imu_data['linear_acceleration.y'], imu_data['linear_acceleration.z']])
+gyr = np.array([imu_data['angular_velocity.x'], imu_data['angular_velocity.y'], imu_data['angular_velocity.z']])
+mag = np.array([mag_data['magnetic_field.x'], mag_data['magnetic_field.y'], mag_data['magnetic_field.z']])
+
+time_acc = imu_data['Time']
+time_mag = mag_data['Time']
+# Setup Sensor
+imu = pps.IMU(acc, gyr, mag, 0)
+imu.mag_cal()
+imu.body_to_nav(range(331))
+
+n = max(imu.acc_n.shape) -2
+quat = np.empty([1, n], dtype=object)
+idx = 0
+for i in range(0, n):
+    if time_mag[idx] - time_acc[i] < 0 and la.norm(imu.mag_n[:,idx]) < mag_th:
+        quat[:,i] = ahrs.madgwick.filter(imu.acc_n[:,i], imu.gyr_n[:,i], mag=imu.mag_n[:,idx], dt=dt, beta=beta, zeta=zeta)
+        idx = idx + 1
+    else:
+        quat[:,i] = ahrs.madgwick.filter(imu.acc_n[:,i], imu.gyr_n[:,i], dt=dt, beta=beta)
+
+att = np.zeros([3,n])
+for i in range(0,n):
+    att[:,i] = quaternion.as_euler_angles(quat[:,i].item())
+
+plt.figure()
+plt.plot(np.transpose(att))
+plt.show()
---
+++ pednav-tools/imu.py
@@ -0,0 +1,96 @@
+import numpy as np
+import scipy as sp
+
+class IMU:
+    def __init__(self, acc, gyr, mag, fs):
+        # Raw Measurements
+        self.raw_acc = acc
+        self.raw_gyr = gyr
+        self.raw_mag = mag
+        self.fs = fs
+        # Calibrated & Filtered Data
+        self.acc_cf = self.raw_acc
+        self.gyr_cf = self.raw_gyr
+        self.mag_cf = self.raw_mag
+        # Rotated, Calibrated, & Filtered Data
+        self.acc_n = np.zeros([3,1])
+        self.gyr_n = np.zeros([3,1])
+        self.mag_n = np.zeros([3,1])
+        print("IMU")
+
+    def mag_cal(self):
+        # Offset Correction
+        off_x = (max(self.raw_mag[0,:]) + min(self.raw_mag[0,:])) / 2
+        off_y = (max(self.raw_mag[1,:]) + min(self.raw_mag[1,:])) / 2
+        off_z = (max(self.raw_mag[2,:]) + min(self.raw_mag[2,:])) / 2
+        # Scale Correction
+        dx_ = (max(self.raw_mag[0,:]) - min(self.raw_mag[0,:])) / 2
+        dy_ = (max(self.raw_mag[1,:]) - min(self.raw_mag[1,:])) / 2
+        dz_ = (max(self.raw_mag[2,:]) - min(self.raw_mag[2,:])) / 2
+        del_ = (dx_ + dy_ + dz_) / 3
+        if dx_ != 0 or dy_ != 0 or dz_ != 0:
+            scale_x = del_ / dx_
+            scale_y = del_ / dy_
+            scale_z = del_ / dz_
+        else:
+            scale_x = 1
+            scale_y = 1
+            scale_z = 1
+
+        self.mag_cf[0,:] = (self.raw_mag[0,:] - off_x) * scale_x
+        self.mag_cf[1,:] = (self.raw_mag[1,:] - off_y) * scale_y
+        self.mag_cf[2,:] = (self.raw_mag[2,:] - off_z) * scale_z
+        print("Mag Cal")
+
+    def body_to_nav(self, range = 0, th=0.01):
+        # Initialize the Navigation Frame Measurements
+        self.acc_n = self.acc_cf
+        self.gyr_n = self.gyr_cf
+        self.mag_n = self.mag_cf
+        # Initial Euler Angle Estimates
+        pitch = np.pi / 2
+        roll = np.pi / 2
+        yaw = 0
+        # Iterative Calculation of Euler Angles to Drive Roll
+        # and Pitch to near 0
+        while pitch > th and roll > th:
+            if self.acc_n.size > 3:
+                acc_ = np.mean(self.acc_n[:,range],axis=1)
+            else:
+                acc_ = self.acc_n
+            roll = np.arctan2(acc_[1], acc_[2]).item()
+            pitch = np.arctan(acc_[0]/np.sqrt(acc_[1]**2 + acc_[2]**2)).item()
+
+            C = self.DCM([roll, pitch, yaw])
+            mag = C@self.mag_cf
+            if mag.size > 3:
+                mag_ = np.mean(mag[:, range],axis=1)
+            else:
+                mag_ = mag
+            yaw = np.arctan2(mag_[1], mag_[0]).item()
+
+            C = self.DCM([roll, pitch, yaw])
+
+            self.acc_n = C@self.acc_n
+            self.gyr_n = C@self.gyr_n
+            self.mag_n = C@self.mag_n
+
+        print("Body 2 Nav")
+
+    def filter(self, freq):
+        print("Filter")
+
+    def DCM(self, angles):
+        ct = np.cos(angles)
+        st = np.sin(angles)
+
+        cz = ct[0]   
+        cx = ct[2]
+        cy = ct[1]
+        sx = st[2]
+        sy = st[1]
+        sz = st[0]
+
+        return np.array([[cy*cz, sy*sx*cz - sz*cx, sy*cx*cz + sz*sx],
+                         [cy*sz, sy*sx*sz + cz*cx, sy*cx*sz - cz*sx],
+                         [  -sy,            cy*sx,            cy*cx]])
