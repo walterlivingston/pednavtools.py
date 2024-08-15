3ac5db6c4499efe705ed3f7fa41bce15869570fd
Walter Livingston <wblivingston18@gmail.com>
2024-08-15 12:05:23 -0500

    setup TODO for ahrs kalman filters

--- pednavtools/ahrs/kalman.py
+++ pednavtools/ahrs/kalman.py
@@ -1,2 +1,14 @@
 import numpy as np
+from ..imu import IMU, Noise
 
+# (TODO) Implement Error-State Kalman Filter
+def eskf(imu:IMU, noise:Noise):
+    pass
+
+# (TODO) Implement Multiplicative Extended Kalman Filter
+def mekf(imu:IMU, noise:Noise):
+    pass
+
+# (TODO) Implement Sequential Multiplicative Extended Kalman Filter
+def smekf(imu:IMU, noise:Noise):
+    pass
